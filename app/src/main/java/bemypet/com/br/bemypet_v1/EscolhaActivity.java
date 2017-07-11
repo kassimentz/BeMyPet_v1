ed05fff9aeb617395690ac11ab3b78efee25eb9e
Kassiane Mentz <kassimentz@gmail.com>
2017-07-11 20:50:21 -0300

    first commit

---
+++ app/src/main/java/bemypet/com/br/bemypet_v1/EscolhaActivity.java
@@ -0,0 +1,13 @@
+package bemypet.com.br.bemypet_v1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EscolhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolha);
+    }
+}
