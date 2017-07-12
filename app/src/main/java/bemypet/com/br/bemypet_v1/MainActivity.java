c2b3b0a5da53b4b22c718821488a17e835536e2f
Kassiane Mentz <kassimentz@gmail.com>
2017-07-12 19:12:31 -0300

    adicionando EscolhaActivity

--- app/src/main/java/bemypet/com/br/bemypet_v1/MainActivity.java
+++ app/src/main/java/bemypet/com/br/bemypet_v1/MainActivity.java
@@ -1,7 +1,9 @@
 package bemypet.com.br.bemypet_v1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    public void iniciar(View v) {
+        Intent intent = new Intent(this, EscolhaActivity.class);
+        startActivity(intent);
     }
 }
