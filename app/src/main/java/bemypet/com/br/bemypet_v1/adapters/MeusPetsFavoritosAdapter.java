cfd21a5dc9d9712e964733e8e57bd01d7ac2a268
kassianesmentz <kassimentz@gmail.com>
2017-09-17 12:13:01 -0300

    listando pets

---
+++ app/src/main/java/bemypet/com/br/bemypet_v1/adapters/MeusPetsFavoritosAdapter.java
@@ -0,0 +1,93 @@
+package bemypet.com.br.bemypet_v1.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bemypet.com.br.bemypet_v1.R;
+import bemypet.com.br.bemypet_v1.pojo.Pet;
+
+/**
+ * Created by kassianesmentz on 17/09/17.
+ */
+
+public class MeusPetsFavoritosAdapter extends BaseAdapter {
+
+    public Context context;
+    public List<Pet> pets;
+
+    public MeusPetsFavoritosAdapter(Context context, List<Pet> pets) {
+        this.context = context;
+        if(pets != null) {
+            this.pets = pets;
+        } else {
+            this.pets = new ArrayList<>();
+        }
+    }
+
+    public class MeusPetsFavoritosHolder {
+        ImageView imgPet;
+        TextView txtNomePet, txtDadosPet;
+    }
+
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return pets.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return pets.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        MeusPetsFavoritosHolder holder;
+
+
+            convertView = LayoutInflater.from(context).inflate(R.layout.row_list_pet, parent, false);
+            holder = new MeusPetsFavoritosHolder();
+            holder.imgPet = (ImageView) convertView.findViewById(R.id.listImgPet);
+            holder.txtNomePet = (TextView) convertView.findViewById(R.id.txtNomePet);
+            holder.txtDadosPet = (TextView) convertView.findViewById(R.id.txtDadosPet);
+
+
+        if(pets.size() > 0) {
+            if (pets.get(i).imagens != null && pets.get(i).imagens.size() > 0) {
+                Glide.with(context).load(pets.get(i).imagens.get(0)).apply(RequestOptions.circleCropTransform()).into(holder.imgPet);
+            }
+
+            holder.txtNomePet.setText(pets.get(i).nome);
+            StringBuilder dadosPet = new StringBuilder();
+            //Espécie (raça), sexo, idade
+            dadosPet.append(pets.get(i).especie);
+            dadosPet.append(" (" + pets.get(i).raca + "), ");
+            dadosPet.append(pets.get(i).sexo + ", ");
+            dadosPet.append(pets.get(i).idadeAproximada);
+            holder.txtDadosPet.setText(dadosPet.toString());
+        }
+
+        return convertView;
+    }
+
+
+}
--- app/src/main/java/bemypet/com/br/bemypet_v1/fragment/MeusPetsFavoritosFragment.java
+++ app/src/main/java/bemypet/com/br/bemypet_v1/fragment/MeusPetsFavoritosFragment.java
@@ -2,6 +2,7 @@ package bemypet.com.br.bemypet_v1.fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,48 +15,53 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.koushikdutta.ion.Ion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import bemypet.com.br.bemypet_v1.CadastroPetActivity;
 import bemypet.com.br.bemypet_v1.FiltrosActivity;
 import bemypet.com.br.bemypet_v1.R;
+import bemypet.com.br.bemypet_v1.adapters.MeusPetsFavoritosAdapter;
+import bemypet.com.br.bemypet_v1.pojo.Pet;
+import bemypet.com.br.bemypet_v1.pojo.Usuario;
+import bemypet.com.br.bemypet_v1.utils.Constants;
 import bemypet.com.br.bemypet_v1.utils.Utils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MeusPetsFavoritosFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MeusPetsFavoritosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MeusPetsFavoritosFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private View rootView;
+    private ListView listViewMeusPets;
+    private ListView listViewPetsFavoritos;
+    private MeusPetsFavoritosAdapter meusPetsAdapter;
+    private MeusPetsFavoritosAdapter petsFavoritosAdapter;
 
+    private List<Pet> meusPets;
+    private List<Pet> meusFavoritos;
+
+    private Usuario usuarioLogado;
 
     private OnFragmentInteractionListener mListener;
 
-    public MeusPetsFavoritosFragment() {
-        // Required empty public constructor
-    }
+    public MeusPetsFavoritosFragment() {}
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MeusPetsFavoritosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MeusPetsFavoritosFragment newInstance(String param1, String param2) {
         MeusPetsFavoritosFragment fragment = new MeusPetsFavoritosFragment();
         Bundle args = new Bundle();
@@ -75,6 +81,31 @@ public class MeusPetsFavoritosFragment extends Fragment {
         }
     }
 
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_meus_pets_favoritos, container, false);
+        listViewMeusPets = (ListView) rootView.findViewById(R.id.listViewMeusPets);
+        listViewPetsFavoritos = (ListView) rootView.findViewById(R.id.listViewPetsFavoritos);
+
+        meusPets = new ArrayList<>();
+        meusFavoritos = new ArrayList<>();
+
+        Usuario tmpUsuario = Utils.getUsuarioSharedPreferences(getContext());
+        if(tmpUsuario != null) {
+            setUsuarioLogado(tmpUsuario);
+            if(getUsuarioLogado().petsFavoritos != null && getUsuarioLogado().petsFavoritos.size() > 0) {
+                meusFavoritos = getUsuarioLogado().petsFavoritos;
+                petsFavoritosAdapter = new MeusPetsFavoritosAdapter(getContext(),meusFavoritos);
+                listViewPetsFavoritos.setAdapter(petsFavoritosAdapter);
+                petsFavoritosAdapter.notifyDataSetChanged();
+            }
+        }
+
+        buscaMeusPets();
+        setHasOptionsMenu(true);
+        return rootView;
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -98,17 +129,6 @@ public class MeusPetsFavoritosFragment extends Fragment {
         return false;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_meus_pets_favoritos, container, false);
-        rootView = inflater.inflate(R.layout.fragment_meus_pets_favoritos, container, false);
-        setHasOptionsMenu(true);
-        return rootView;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -129,18 +149,38 @@ public class MeusPetsFavoritosFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void buscaMeusPets() {
+        //buscar os pets do usuario e setar no adapter
+
+        FirebaseDatabase.getInstance().getReference().child("pets").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Pet pet = null;
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    pet = postSnapshot.getValue(Pet.class);
+                    if(pet.atualDonoID.equalsIgnoreCase(getUsuarioLogado().id)) {
+                        meusPets.add(pet);
+                    }
+                }
+                meusPetsAdapter = new MeusPetsFavoritosAdapter(getContext(), meusPets);
+                listViewMeusPets.setAdapter(meusPetsAdapter);
+                meusPetsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+    }
+
+    public Usuario getUsuarioLogado() {
+        return usuarioLogado;
+    }
+
+    public void setUsuarioLogado(Usuario usuarioLogado) {
+        this.usuarioLogado = usuarioLogado;
     }
 }
