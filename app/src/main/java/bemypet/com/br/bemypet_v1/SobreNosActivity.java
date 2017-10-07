9f0116d827a739437380ab404610c9ee1f26901e
kassianesmentz <kassimentz@gmail.com>
2017-10-07 00:24:50 -0300

    adicionando o numero da versao na activity sobre

--- app/src/main/java/bemypet/com/br/bemypet_v1/SobreNosActivity.java
+++ app/src/main/java/bemypet/com/br/bemypet_v1/SobreNosActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
 import bemypet.com.br.bemypet_v1.pojo.Pet;
 import bemypet.com.br.bemypet_v1.utils.Utils;
+import bemypet.com.br.bemypet_v1.BuildConfig;
 
 public class SobreNosActivity extends AppCompatActivity {
 
@@ -24,6 +26,12 @@ public class SobreNosActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
         ab.setTitle(R.string.activity_title_sobre);
+
+        int versionCode = BuildConfig.VERSION_CODE;
+        String versionName = BuildConfig.VERSION_NAME;
+
+        TextView txtVersao = (TextView) findViewById(R.id.txtVersao);
+        txtVersao.setText("Versão: "+versionName);
     }
 
 
